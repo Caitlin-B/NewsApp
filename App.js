cda8eb01d445a65b9768e635f8ec648495311de6
Caitlin-B <56935291+Caitlin-B@users.noreply.github.com>
2020-04-23 12:03:56 +0100

    initial setup

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Look I have made a mobile app, yippee for me!</Text>
     </View>
   );
 }
